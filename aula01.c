ed9e6a11fad0463b335ca2168e7e673a8ad087b4
lucasnative <santos.lucasdanielsp@gmail.com>
2021-03-11 19:37:16 -0300

    Add lesions

---
+++ aula01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int ret = 0;
+  
+  printf("hello aula 01\n\n");
+  printf("%d - %c - %d - %d\n", ret, 10, 0xa, 'A');
+  return 0;
+}
+
---
+++ char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  char c;
+  
+  
+  printf("O tamanho da variável C é: %lu\n", sizeof c);
+  
+  return 0;
+}

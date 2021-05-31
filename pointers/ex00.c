54d0420bee33a966e89bb7ccedf9ae42c258c868
lucasnative <santos.lucasdanielsp@gmail.com>
2021-05-31 13:02:17 -0300

    Create directory pointers with first ex

---
+++ pointers/ex00.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+// re-coded standard putchar();
+void ft_putchar(char c)
+{
+	write(1, &c, 2);
+}
+
+// Function using ft_putchar, with pointer arithmetic, for print a string;
+void print(char *s)
+{
+	while (*s)
+		ft_putchar(*s++);
+}
+
+// 
+int main(void)
+{
+	char my_string[] = {72,101,108,108,111,32, 0x57, 0x6F, 0x72, 0x6C, 0x64,0x21, '\n'};
+	print(my_string);
+}
